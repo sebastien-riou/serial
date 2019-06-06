f16fc34123731adcaf5047d6a57d87244bdde2c3
sebastien-riou <sriou@nimp.co.uk>
2019-06-06 21:25:43 +0200

    linux port

--- inc/serial.h
+++ inc/serial.h
@@ -11,14 +11,14 @@ static int serial_open(serial_t *ctx, const char*port){
     char buf[1024]={0};
     strcpy(buf,"\\\\.\\");
     strncat(buf,port,sizeof(buf)-5);
-    *ctx = CreateFile(buf,            //port name 
-        GENERIC_READ | GENERIC_WRITE, //Read/Write   				 
-        0,            // No Sharing                               
-        NULL,         // No Security                              
-        OPEN_EXISTING,// Open existing port only                     
-        0,            // Non Overlapped I/O                           
+    *ctx = CreateFile(buf,            //port name
+        GENERIC_READ | GENERIC_WRITE, //Read/Write
+        0,            // No Sharing
+        NULL,         // No Security
+        OPEN_EXISTING,// Open existing port only
+        0,            // Non Overlapped I/O
         NULL);        // Null for Comm Devices
-                         
+
     if (*ctx == INVALID_HANDLE_VALUE) return 1;
     return 0;
 }
@@ -41,14 +41,38 @@ static void serial_delay_ms(unsigned int ms){
 }
 #else
 
-//POSIX
+//Linux
+#include <time.h>
 #include <unistd.h>
-#include <fcntl.h> 
+#include <fcntl.h>
 #include <termios.h>
-typedef serial_t int;
+#include <sys/ioctl.h>
+typedef int serial_t;
+static int serial_open(serial_t *ctx, const char*port){
+    *ctx = open (port, O_RDWR | O_NOCTTY);
+    return *ctx;
+}
+static void serial_close(serial_t*ctx){
+    close(*ctx);//Closing the Serial Port
+}
 
+static int serial_dtr(serial_t*ctx,int level){
+    int flag = TIOCM_DTR;
+    return ioctl(*ctx,level ? TIOCMBIS : TIOCMBIC,&flag);
+}
+
+static int serial_rts(serial_t*ctx,int level){
+    int flag = TIOCM_RTS;
+    return ioctl(*ctx,level ? TIOCMBIS : TIOCMBIC,&flag);
+}
 static void serial_delay_ms(unsigned int ms){
     usleep(ms*1000);
+/*
+        struct timespec time;
+        time.tv_sec =0;
+        time.tv_nsec =1000 * 1000 * (uint64_t)ms;
+        nanosleep(&time, NULL);
+*/
 }
 #endif
 
@@ -60,4 +84,3 @@ static int serial_rts_pulse_ms(serial_t*ctx,unsigned int ms){
 }
 
 #endif
-
--- tests/main.c
+++ tests/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "serial.h"
-    
+
 int main(int argc, char*argv[]){
     if(argc!=2){
         printf("ERROR: Need exactly one argument: the COM port name\n");
@@ -8,10 +8,12 @@ int main(int argc, char*argv[]){
     }
     printf("openning port %s\n",argv[1]);
     serial_t ctx_storage;serial_t*ctx=&ctx_storage;
-    if (serial_open(ctx,argv[1]))
-        printf("Error in opening serial port %s\n",argv[1]);		
-    else
+    if (serial_open(ctx,argv[1])){
+        printf("Error in opening serial port %s\n",argv[1]);
+        //return -2; //skipping return allows testing on linux even when the device access is denied!
+    } else {
         printf("opening serial port successful\n");
+    }
     printf("Press the following keys:\n");
     printf("1: Set DTR\n");
     printf("2: Clear DTR\n");
@@ -19,24 +21,24 @@ int main(int argc, char*argv[]){
     printf("4: Clear RTS\n");
     printf("5: 200ms Pulse on RTS\n");
     printf("q: End Program\n");
-    
+
     uint8_t ch;
     do
-    {   
+    {
         fflush(stdout);
         ch = getchar();
         switch (ch)
         {
-        case '1':   if (serial_dtr(ctx,1)) 
+        case '1':   if (serial_dtr(ctx,1))
                         printf ("Error Setting DTR\n");
                     break;
-        case '2':   if (serial_dtr(ctx,0)) 
+        case '2':   if (serial_dtr(ctx,0))
                         printf ("Error Clearing DTR\n");
                     break;
-        case '3':   if (serial_rts(ctx,1)) 
+        case '3':   if (serial_rts(ctx,1))
                         printf ("Error Setting RTS\n");
                     break;
-        case '4':   if (serial_rts(ctx,0)) 
+        case '4':   if (serial_rts(ctx,0))
                         printf ("Error Clearing RTS\n");
                     break;
         case '5':   if(serial_rts_pulse_ms(ctx,200))
